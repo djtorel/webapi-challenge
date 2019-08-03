e8c0c9850df6979f9a786a19eabe5b98c1fcd65b
Dominic Torruellas <dom@domtorr.com>
2019-08-03 11:44:55 -0700

    GET - /actions - Get all actions

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
+// Import action database helpers
+const actionDb = require('../data/helpers/actionModel');
+
+router.get('/', (req, res, next) => {
+  actionDb
+    .get()
+    .then(actions => res.status(200).json(actions))
+    .catch(next);
+});
+
 module.exports = router;
