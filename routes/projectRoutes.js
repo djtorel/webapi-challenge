f1008c96dfeba02c119629a632479b344ed74692
Dominic Torruellas <dom@domtorr.com>
2019-08-03 12:54:00 -0700

    GET - /projects/:id - Get project by ID

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,12 +1,25 @@
 const router = require('express').Router();
 
+// Import project database helpers
 const projectDb = require('../data/helpers/projectModel');
 
+// Import middleware
+const { validate } = require('../middleware');
+
+// --- Get routes start ---
+// GET all projects
 router.get('/', (req, res, next) => {
   projectDb
     .get()
     .then(projects => res.status(200).json(projects))
     .catch(next);
 });
+
+// GET project by ID
+router.get('/:id', validate.projectId, (req, res) => {
+  res.status(200).json(req.project);
+});
+
+// --- Get routes end ---
 
 module.exports = router;
