6e3b8d9e80b923f4c633e372064636df170d8af9
Dominic Torruellas <dom@domtorr.com>
2019-08-03 11:42:56 -0700

    GET - /projects - Get all projects

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
 
+const projectDb = require('../data/helpers/projectModel');
+
+router.get('/', (req, res, next) => {
+  projectDb
+    .get()
+    .then(projects => res.status(200).json(projects))
+    .catch(next);
+});
+
 module.exports = router;
