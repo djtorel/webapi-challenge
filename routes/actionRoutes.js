58c01dc80e113b92ecfaccd33af5a39c302b35ec
Dominic Torruellas <dom@domtorr.com>
2019-08-03 13:14:57 -0700

    GET - /actions/:id - Get action by ID

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -3,11 +3,23 @@ const router = require('express').Router();
 // Import action database helpers
 const actionDb = require('../data/helpers/actionModel');
 
+// Import middleware
+const { validate } = require('../middleware');
+
+// --- Get routes start ---
+// GET all actions
 router.get('/', (req, res, next) => {
   actionDb
     .get()
     .then(actions => res.status(200).json(actions))
     .catch(next);
 });
+
+// Get action by ID
+router.get('/:id', validate.actionId, (req, res) => {
+  res.status(200).json(req.action);
+});
+
+// --- Get routes end ---
 
 module.exports = router;
