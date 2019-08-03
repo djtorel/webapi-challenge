7310db2dfe8dc657f1719eebcedfcd5921f30900
Dominic Torruellas <dom@domtorr.com>
2019-08-03 13:14:31 -0700

    validate.actionID middleware added

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,5 +1,6 @@
 // Import project database helpers
 const projectDb = require('../data/helpers/projectModel');
+const actionDb = require('../data/helpers/actionModel');
 
 // Import utilities
 const { error } = require('../utils');
@@ -18,6 +19,21 @@ const projectId = (req, res, next) => {
   });
 };
 
+// Action ID validator middleware
+const actionId = (req, res, next) => {
+  const { id } = req.params;
+
+  actionDb.get(id).then(action => {
+    if (action) {
+      req.action = action;
+      next();
+    } else {
+      next(error(400, 'invalid action id'));
+    }
+  });
+};
+
 module.exports = {
   projectId,
+  actionId,
 };
